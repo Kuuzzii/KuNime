b35189d8e404ec15d362d911da565353c2b86ae7
Kuuzzii <betlogpapamo123@gmail.com>
2025-04-24 03:03:52 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 const API_KEY = '961334ce43e0adcaa714fddec89fcfd9';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/original';
+let currentItem; // Global variable to hold the currently selected movie/show
 
 // This function will be called when the Play button is clicked
 function goToMoviePage() {
