1be5fe9592863250ec5ad6760e61c2202c46e718
Kuuzzii <betlogpapamo123@gmail.com>
2025-04-24 15:21:19 +0800

    Update watch.js

--- watch.js
+++ watch.js
@@ -1,6 +1,5 @@
 const API_KEY = '961334ce43e0adcaa714fddec89fcfd9';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const IMG_URL = 'https://image.tmdb.org/t/p/original';
 
 // Extract query parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
